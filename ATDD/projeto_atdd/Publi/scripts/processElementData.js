e5009cc57fb0590cb40df2e80b91585c691a489c
Wanderson Costa da Silva <wanderson.c@academico.ifrn.edu.br>
2019-11-27 23:13:11 -0300

    up projeto

---
+++ ATDD/projeto_atdd/Publi/scripts/processElementData.js
@@ -0,0 +1,52 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_ZWHFkBA8EeqLiqRqsqRnPAActivity"]="./../../projeto_atdd/deliveryprocesses/resources/Debater os Requisitos_2C8C455_2b830714_Activity.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_ZWHFkBA8EeqLiqRqsqRnPAActivityDetail"]="./../../projeto_atdd/deliveryprocesses/resources/Debater os Requisitos_2C8C455_2b830714_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_bgerYRA8EeqLiqRqsqRnPAActivity"]="./../../projeto_atdd/deliveryprocesses/resources/Refinar os Testes de Aceitação_CF80012E_f83a43ed_Activity.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_bgerYRA8EeqLiqRqsqRnPAActivityDetail"]="./../../projeto_atdd/deliveryprocesses/resources/Refinar os Testes de Aceitação_CF80012E_f83a43ed_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_YZpdURA8EeqLiqRqsqRnPAActivity"]="./../../projeto_atdd/deliveryprocesses/resources/Apresentar os resultados dos testes de aceitação_13634F15_3c1d91d4_Activity.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_YZpdURA8EeqLiqRqsqRnPAActivityDetail"]="./../../projeto_atdd/deliveryprocesses/resources/Apresentar os resultados dos testes de aceitação_13634F15_3c1d91d4_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_ailfwBA8EeqLiqRqsqRnPAActivity"]="./../../projeto_atdd/deliveryprocesses/resources/Implementar o código_121718B0_3ad15b6f_Activity.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPA,_ailfwBA8EeqLiqRqsqRnPAActivityDetail"]="./../../projeto_atdd/deliveryprocesses/resources/Implementar o código_121718B0_3ad15b6f_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_mY87sRA6EeqLiqRqsqRnPAActivity"]="./../../projeto_atdd/deliveryprocesses/resources/ciclo_C44856B_c44856b_Activity.jpeg"
+contentPage.processPage.elementUrls["_s6vqMBA5EeqLiqRqsqRnPA"] = ["organizar os testes de aceitação","projeto_atdd/capabilitypatterns/organizar_os_testes_de_aceitacao_9A288A53.html"];
+contentPage.processPage.elementUrls["_0UiiQBA5EeqLiqRqsqRnPA"] = ["testes unitários","projeto_atdd/capabilitypatterns/testes_unitarios_CD6B04F6.html"];
+contentPage.processPage.elementUrls["_8x03oBA5EeqLiqRqsqRnPA"] = ["Product Owner","projeto_atdd/capabilitypatterns/product_owner_CEA19056.html"];
+contentPage.processPage.elementUrls["_oiLHcRA5EeqLiqRqsqRnPA"] = ["Refinar os Testes de Aceitação","projeto_atdd/capabilitypatterns/Refinar os Testes de Aceitação_DB03C5.html"];
+contentPage.processPage.elementUrls["_5r62oRA5EeqLiqRqsqRnPA"] = ["Apresentar os resultados dos testes de aceitação","projeto_atdd/capabilitypatterns/Apresentar os resultados dos testes de aceitação_9A3923FC.html"];
+contentPage.processPage.elementUrls["_fVGT4hA5EeqLiqRqsqRnPA"] = ["Product Owner","projeto_atdd/capabilitypatterns/product_owner_4C713131.html"];
+contentPage.processPage.elementUrls["_fVGT4BA5EeqLiqRqsqRnPA"] = ["equipe multifuncional","projeto_atdd/capabilitypatterns/equipe_multifuncional_533A3D0B.html"];
+contentPage.processPage.elementUrls["_fVHiABA5EeqLiqRqsqRnPA"] = ["historia de usuario","projeto_atdd/capabilitypatterns/historia_de_usuario_153E93D2.html"];
+contentPage.processPage.elementUrls["_0UkXcBA5EeqLiqRqsqRnPA"] = ["condições propostas","projeto_atdd/capabilitypatterns/condicoes_propostas_C6A01A11.html"];
+contentPage.processPage.elementUrls["_ZWHFkBA8EeqLiqRqsqRnPA"] = ["Debater os Requisitos","projeto_atdd/deliveryprocesses/Debater os Requisitos_2C8C455.html"];
+contentPage.processPage.elementUrls["_fVG68hA5EeqLiqRqsqRnPA"] = ["perguntas","projeto_atdd/capabilitypatterns/perguntas_44C1450F.html"];
+contentPage.processPage.elementUrls["_8x1esxA5EeqLiqRqsqRnPA"] = ["testes realizados","projeto_atdd/capabilitypatterns/testes_realizados_F7A6DB19.html"];
+contentPage.processPage.elementUrls["_fVG68BA5EeqLiqRqsqRnPA"] = ["outros interessado","projeto_atdd/capabilitypatterns/outro_4B8A50E9.html"];
+contentPage.processPage.elementUrls["_mY87sRA6EeqLiqRqsqRnPA"] = ["ciclo","projeto_atdd/deliveryprocesses/ciclo_C44856B.html"];
+contentPage.processPage.elementUrls["_w3-70RA5EeqLiqRqsqRnPA"] = ["Implementar o código","projeto_atdd/capabilitypatterns/Implementar o código_AECB9008.html"];
+contentPage.processPage.elementUrls["_0UkXdBA5EeqLiqRqsqRnPA"] = ["testes executados","projeto_atdd/capabilitypatterns/testes_executados_8B31FC30.html"];
+contentPage.processPage.elementUrls["_8xzpgBA5EeqLiqRqsqRnPA"] = ["Verificar a História","projeto_atdd/capabilitypatterns/verificar_a_historia_A073C1D1.html"];
+contentPage.processPage.elementUrls["_s6xfYhA5EeqLiqRqsqRnPA"] = ["teste automatizado","projeto_atdd/capabilitypatterns/teste_automatizado_C3279160.html"];
+contentPage.processPage.elementUrls["_0UkXchA5EeqLiqRqsqRnPA"] = ["expectativas existentes","projeto_atdd/capabilitypatterns/expectativas_existentes_BFD70E37.html"];
+contentPage.processPage.elementUrls["_bgerYRA8EeqLiqRqsqRnPA"] = ["Refinar os Testes de Aceitação","projeto_atdd/deliveryprocesses/Refinar os Testes de Aceitação_CF80012E.html"];
+contentPage.processPage.elementUrls["_8x1esRA5EeqLiqRqsqRnPA"] = ["equipe multifuncional","projeto_atdd/capabilitypatterns/equipe_multifuncional_FE6FE6F3.html"];
+contentPage.processPage.elementUrls["_fVFs0BA5EeqLiqRqsqRnPA"] = ["histórias de usuário","projeto_atdd/capabilitypatterns/historias_de_usuario_40F2B48F.html"];
+contentPage.processPage.elementUrls["_s6xfYBA5EeqLiqRqsqRnPA"] = ["historia de usuario","projeto_atdd/capabilitypatterns/historia_de_usuario_C9F09D3A.html"];
+contentPage.processPage.elementUrls["_YZpdURA8EeqLiqRqsqRnPA"] = ["Apresentar os resultados dos testes de aceitação","projeto_atdd/deliveryprocesses/Apresentar os resultados dos testes de aceitação_13634F15.html"];
+contentPage.processPage.elementUrls["_ailfwBA8EeqLiqRqsqRnPA"] = ["Implementar o código","projeto_atdd/deliveryprocesses/Implementar o código_121718B0.html"];
+contentPage.processPage.elementUrls["_8x2FwxA5EeqLiqRqsqRnPA"] = ["nova história","projeto_atdd/capabilitypatterns/nova_historia_9EE6395.html"];
+contentPage.processPage.elementUrls["_Y-1VYRA5EeqLiqRqsqRnPA"] = ["Debater os Requisitos","projeto_atdd/capabilitypatterns/Debater os Requisitos_A1105732.html"];
+contentPage.processPage.elementUrls["_0UjwYBA5EeqLiqRqsqRnPA"] = ["equipe multifuncional","projeto_atdd/capabilitypatterns/equipe_multifuncional_18ED44DB.html"];
+contentPage.processPage.elementUrls["_8x2FwRA5EeqLiqRqsqRnPA"] = ["alteração nos testes existentes","projeto_atdd/capabilitypatterns/alteracao_nos_testes_existentes_10B76F6F.html"];
+contentPage.processPage.elementUrls["_s6w4UBA5EeqLiqRqsqRnPA"] = ["equipe multifuncional","projeto_atdd/capabilitypatterns/equipe_multifuncional_A4C026AD.html"];
